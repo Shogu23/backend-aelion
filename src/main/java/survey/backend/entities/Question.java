6359a4f8369876f3e64fc765493d3a6accd43b5c
Shogu23 <charles.marthouret@gmail.com>
2023-01-10 15:18:16 +0100

    entities Survey, Question

--- src/main/java/survey/backend/entities/Question.java
+++ src/main/java/survey/backend/entities/Question.java
@@ -2,28 +2,22 @@ package survey.backend.entities;
 
 import lombok.Getter;
 import lombok.Setter;
-import survey.backend.enums.Level;
-import survey.backend.enums.PoeType;
+import survey.backend.enums.AnswerType;
 
 import javax.persistence.*;
 
 @Getter @Setter
 @Entity
-public class Survey {
+public class Question {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
-    private String title;
+    private String text;
 
-    @Column(length = 10, nullable = false)
+    @Column(length = 11, nullable = false)
     @Enumerated(EnumType.STRING)
-    private Level level;
-
-    @Column(length = 4)
-    @Enumerated(EnumType.STRING)
-    private PoeType type;
-
+    private AnswerType answerType;
 }
