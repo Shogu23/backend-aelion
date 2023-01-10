0c104685508469fc004ae020b4d96ab780b7963d
Shogu23 <charles.marthouret@gmail.com>
2023-01-10 14:35:15 +0100

    babystep

---
+++ src/main/java/survey/backend/entities/Survey.java
@@ -0,0 +1,11 @@
+package survey.backend.entities;
+
+import survey.backend.enums.PoeType;
+
+public class Survey {
+    private Long id;
+    private String title;
+    private String level;
+    private PoeType type;
+    private String answerType;
+}
